5c7c8dbdfb42ba0611f81685f1989000710b4cdc
Xinpeng Liu <37204561+davidlxp@users.noreply.github.com>
2022-03-08 14:20:24 -0500

    Latest Version before big modification

--- main.cpp
+++ main.cpp
@@ -1,10 +1,22 @@
 
 # include <iostream>
+# include <vector>
 # include "Stack.cpp"
 
-// printStack pop all items from a Stack and print all of them
+/**
+* @Brief printStack pop all items from a Stack and print all of them
+*/
 template <class T>
-void printStack(Stack<T>& stack);
+void printStack(Stack<T>&);
+
+/**
+ * @Brief getPrecedence returns the precedence of an operator (eg. '+')
+ * @Input 'op' is the operator the user wants to check precedence for.
+ * 'opPrecedence' a vector of operators with user defined precedence.
+ * operator which listed on the rearer part of the vector has higher precedence.
+ * @Output the precedence of an operator
+ */
+int getPrecedence(char&, vector<char>&);
 
 
 int main() {
@@ -18,9 +30,10 @@ int main() {
     bool opFlag = false;                            // flag shows the item read previously is an operator
     bool beginFlag = true;                          // flag shows it's the beginning of reading
 
+    // a vector of operators, ordered by their precedence. The operator with the highest precedence is at the rear.
+    vector<char> precedence { '+', '-', '*', '/' };
 
     cout << "Please Provide Arithmetic Evaluation: ";
-
     while (cin.peek() != '\n')
     {
         // 1. skip the whitespace if any
@@ -47,14 +60,22 @@ int main() {
 
             opFlag = true;                          // the last read is an operator so opFlag turns to True
             beginFlag = false;                      // once read something, it's not beginning of expression anymore
-
-            cout << "done" << endl;
         }
-        // 4. if peek an operator, and in front of it there's another operator which is not ')', and this operator
-        //    is not '(', then this operator will be read along with the number behind it like '+8' or '-8'
-        else if (!beginFlag && opFlag && op!=')' && cin.peek() != '(')
+        // 4. if peek an operator, and in front of it there's another operator which is not ')', and current operator
+        //    is '-' or '+', then this operator will be read along with the number behind it like '+8' or '-8'
+        else if (!beginFlag && opFlag && op!=')' && (cin.peek() == '-' || cin.peek() == '+'))
         {
-            cin >> num;                             // read the operator with the number behind it
+            char tempOp;                            // save '-' or '+' to a temp operator
+            cin >> tempOp;                          // read and get the tempOp
+
+            while (cin.peek() == ' ')               // skip the whitespace (if any) between operator and the number
+                cin.ignore();
+
+            cin >> num;                             // read the pure number (without sign)
+
+            if (tempOp == '-')                      // assign negativity to the number based on saved operator
+                num = 0 - num;
+
             numStack.push(num);                     // push the number to stack
 
             opFlag = false;                         // read a number, it's not operator, so opFlag turns to False
@@ -69,7 +90,7 @@ int main() {
             opFlag = false;                         // read a number, it's not operator, so opFlag turns to False
             beginFlag = false;                      // once read a number, it's not beginning of expression anymore
         }
-        // 5. if meet operators under the other situation which not stated above
+        // 6. if meet operators under the other situation which are not stated above
         else
         {
             cin >> op;
@@ -89,6 +110,29 @@ int main() {
 
     return 0;
 }
+
+template <class T>
+int getPrecedence(char& op, vector<char>& opPrecedence)
+{
+    int res = -1;
+    for (int i=0; i<opPrecedence.size(); ++i)
+    {
+        if (op == opPrecedence[i])
+        {
+            res = i;
+            break;
+        }
+    }
+    return res;
+}
+
+//template <class T>
+//bool isLessPrecedence(char& leftOp, char& rightOp, vector<char>& opPrecedence)
+//{
+//    bool res = false;
+//
+//
+//}
 
 template <class T>
 void printStack(Stack<T>& stack)
