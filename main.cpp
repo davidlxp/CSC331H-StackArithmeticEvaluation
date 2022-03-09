e0d693c7ad26d87077902cc59f810dc6be1ff9c1
Xinpeng Liu <37204561+davidlxp@users.noreply.github.com>
2022-03-09 01:42:56 -0500

    Updated the algorithm to "-7" -> "-1 * 7", a success!

--- main.cpp
+++ main.cpp
@@ -48,57 +48,56 @@ int main() {
             numStack.push(num);
 
             opFlag = false;                         // read a number, it's not operator, so opFlag turns to False
-            beginFlag = false;                      // once read a number, it's not beginning of expression anymore
-
-        }
-        // 3. if it's the beginning, and start with '-' or '+'
-        else if (beginFlag && (cin.peek() == '-' || cin.peek() == '+'))
-        {
-            numStack.push(0);                  // push a 0 (eg. turn "-3+4" into form of "0-3+4")
-            cin >> op;                              // read and push the operator '+' or '-'
-            opStack.push(op);
-
-            opFlag = true;                          // the last read is an operator so opFlag turns to True
             beginFlag = false;                      // once read something, it's not beginning of expression anymore
         }
-        // 4. if peek an operator, and in front of it there's another operator which is not ')', and current operator
-        //    is '-' or '+', then this operator will be read along with the number behind it like '+8' or '-8'
-        else if (!beginFlag && opFlag && op!=')' && (cin.peek() == '-' || cin.peek() == '+'))
+        // 3. when see '-' or '+' operator
+        //    --> and if it's the beginning, or it's in the middle but right after an operator,
+        //    which is not ')', we turn the form like '3+ -  2' into '3+ (-1) * 2'.
+        //    --> if the scenarios stated above are not satisfied, read as an ordinary '+' or '-'
+        else if (cin.peek() == '-' || cin.peek() == '+')
         {
-            char tempOp;                            // save '-' or '+' to a temp operator
-            cin >> tempOp;                          // read and get the tempOp
+            if (beginFlag || (opFlag && op != ')')) // when '+' and '-' indicate a number's negativity
+            {
+                cin >> op;                          // read the operator '-' or '+'
 
-            while (cin.peek() == ' ')               // skip the whitespace (if any) between operator and the number
-                cin.ignore();
+                if (op == '-')                      // operator '-' means '-1 *'
+                    numStack.push(-1);         // add number '-1' to number stack
+                else                                // operator '+' means '1 *'
+                    numStack.push(1);         // add number '1' to number stack
 
-            cin >> num;                             // read the pure number (without sign)
+                opStack.push('*');             // add operator '*' to stack
 
-            if (tempOp == '-')                      // assign negativity to the number based on saved operator
-                num = 0 - num;
-
-            numStack.push(num);                     // push the number to stack
-
-            opFlag = false;                         // read a number, it's not operator, so opFlag turns to False
-            beginFlag = false;                      // once read a number, it's not beginning of expression anymore
+                opFlag = true;                      // the last read is an operator '*', so opFlag turns to True
+                beginFlag = false;                  // once read something, it's not beginning of expression anymore
+            }
+            else                                    // when '-' or '+' play role as normal arithmetic operator
+            {
+                cin >> op;
+                opStack.push(op);
+                opFlag = true;                      // read an operator, turns opFlag to True
+                beginFlag = false;                  // once read something, it's not beginning of expression anymore
+            }
         }
-        // 5. if ever read '.', it must be a part of a decimal number like '.6' = '0.6'
+        // 4. if ever read '.', it must be a part of a decimal number like '.6' = '0.6'
+        //    we change the form like '.5' to '0.1 * 5'
         else if (cin.peek() == '.')
         {
-            cin >> num;                             // read '.' together with the number behind it
-            numStack.push(num);                     // push the number to stack
+            cin >> op;                              // read the decimal so the input stream can move on
 
-            opFlag = false;                         // read a number, it's not operator, so opFlag turns to False
-            beginFlag = false;                      // once read a number, it's not beginning of expression anymore
+            numStack.push(0.1);                // push 0.1 to the number stack
+            opStack.push('*');                 // push '*' to the operator stack
+
+            opFlag = true;                          // the last read is an operator '*', so opFlag turns to True
+            beginFlag = false;                      // once read something, it's not beginning of expression anymore
         }
-        // 6. if meet operators under the other situation which are not stated above
+        // 5. if meet operators under the other situation which are not stated above
+        //    read they are normal operators
         else
         {
             cin >> op;
-
             opStack.push(op);
-
             opFlag = true;                          // read an operator, turns opFlag to True
-            beginFlag = false;                      // once read an operator, it's not beginning of expression anymore
+            beginFlag = false;                      // once read something, it's not beginning of expression anymore
         }
     }
 
